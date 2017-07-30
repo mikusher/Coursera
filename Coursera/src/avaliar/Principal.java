15383090c621ff5f47c05c5b6220c283ed85a7c8
Luis Tavares <mikusher@hotmail.com>
2017-07-30 02:06:40 +0100

    Merge origin/master
    Conflicts:
    	src/cv/mikusher/databaseMoment/ColumnHeaders.java
    	src/cv/mikusher/databaseMoment/DataSourceEx.java
    	src/cv/mikusher/databaseMoment/MultipleStatements.java
    	src/cv/mikusher/databaseMoment/NotPrepared.java
    	src/cv/mikusher/databaseMoment/Prepared.java
    	src/cv/mikusher/databaseMoment/Prepared2.java
    	src/cv/mikusher/databaseMoment/RetrieveAll.java

--- Coursera/src/avaliar/Principal.java
+++ Coursera/src/avaliar/Principal.java
@@ -1,33 +1,25 @@
 
-
-
-
-
 package avaliar;
-
-
-
-
 
 public class Principal {
 
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
+	// TODO Auto-generated method stub
 
-        Paciente p1 = new Paciente(100, 1.84);
-        Paciente p2 = new Paciente(56.9, 1.76);
-        Paciente p3 = new Paciente(105.4, 1.67);
+	Paciente p1 = new Paciente(100, 1.84);
+	Paciente p2 = new Paciente(56.9, 1.76);
+	Paciente p3 = new Paciente(105.4, 1.67);
 
-        p1.calcularIMC();
-        p2.calcularIMC();
-        p3.calcularIMC();
+	p1.calcularIMC();
+	p2.calcularIMC();
+	p3.calcularIMC();
 
-        System.out.println("IMC = " + p1.calcularIMC());
-        System.out.println(p1.diagnostico() + "\n");
-        System.out.println("IMC = " + p2.calcularIMC());
-        System.out.println(p2.diagnostico() + "\n");
-        System.out.println("IMC = " + p3.calcularIMC());
-        System.out.println(p3.diagnostico());
+	System.out.println("IMC = " + p1.calcularIMC());
+	System.out.println(p1.diagnostico() + "\n");
+	System.out.println("IMC = " + p2.calcularIMC());
+	System.out.println(p2.diagnostico() + "\n");
+	System.out.println("IMC = " + p3.calcularIMC());
+	System.out.println(p3.diagnostico());
 
     }
 
